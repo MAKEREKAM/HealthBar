456a1b6872aedbe5c09b9dd87976b5be156505c1
MAKEREKAM <kimyounghyun123456@gmail.com>
2023-09-28 21:46:58 +0900

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 group = "kr.vanilage"
 version = "1.0" // 여기에 버전을 작성하세요.
 
-var buildPath = "D:\\Programming\\Mcserver\\연구소\\plugins\\update" // 빌드 주소를 설정하세요.
+var buildPath = "" // 빌드 주소를 설정하세요.
 
 repositories {
     mavenCentral()
@@ -13,6 +13,7 @@ repositories {
 }
 
 dependencies {
+    testImplementation(kotlin("test"))
     compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT"); // 여기 버전을 바꿀수 있습니다.
 }
 
